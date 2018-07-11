3ea5ebab1878537a3d9525fc9304e6370600afae
In Soo Yang <isyang1223@gmail.com>
2018-07-11 16:51:31 -0700

    added navbar and some routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import OurMission from "./components/pages/OurMission";
+
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={Landing} />
+          <div className="container">
+            <Route exact path="/ourmission" component={OurMission} />
+          
+          
+          
+          
+          
+          
+          
+          
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ client/src/components/pages/OurMission.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class OurMission extends Component {
+  render() {
+    return (
+        <div className="ourmission">
+            <h1>ourmission!!!!!</h1>
+          
+        </div>
+    )
+  }
+}
+
+
+export default OurMission;
