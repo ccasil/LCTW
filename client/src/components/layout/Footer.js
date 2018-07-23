09022414513ec24f8296c884867bdb655cc193e6
ccasil <cesarkylecasil@gmail.com>
2018-07-23 16:16:13 -0700

    modularize css files

--- client/src/components/layout/Footer.js
+++ client/src/components/layout/Footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import "../css/Footer.css";
 
 class Footer extends Component {
     render() {
--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
 import logo from '../../images/logo.png';
+import "../css/Landing.css";
 
 class Landing extends Component {
   render() {
