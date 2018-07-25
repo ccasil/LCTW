3594afbb0a9c9fcda77c7b078ea2ed16c0f5921c
ccasil <cesarkylecasil@gmail.com>
2018-07-25 14:15:09 -0700

    merged and edited part of landing

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -19,33 +19,33 @@ class Landing extends Component {
           <h1>Follow Our Movement</h1>
           <div className="row">
             <div className="col-md-3 col-sm-12">
-              <a href="https://www.facebook.com/lovechangingtheworld" target="_blank">
+              <a className="socialmedialink" href="https://www.facebook.com/lovechangingtheworld" target="_blank">
                 <i className="fab fa-facebook-square" />
               </a>
               <p>Facebook</p>
             </div>
             <div className="col-md-3 col-sm-12">
-              <a href="https://twitter.com/LCTW_" target="_blank">
+              <a className="socialmedialink" href="https://twitter.com/LCTW_" target="_blank">
                 <i className="fab fa-twitter-square" />
               </a>
               <p>Twitter</p>
             </div>
             <div className="col-md-3 col-sm-12">
-              <a href="http://www.instagram.com/_lovechangingtheworld_" target="_blank">
+              <a className="socialmedialink" href="http://www.instagram.com/_lovechangingtheworld_" target="_blank">
                 <i className="fab fa-instagram" />
               </a>
               <p>Instagram</p>
             </div>
             <div className="col-md-3 col-sm-12">
-              <a href="https://www.snapchat.com/add/lctw1" target="_blank">
+              <a className="socialmedialink" href="https://www.snapchat.com/add/lctw1" target="_blank">
                 <i className="fab fa-snapchat-square" />
               </a>
               <p>Snapchat</p>
             </div>
           </div>
 
-          <p>donations</p>
-          <button target="_blank" href="https://www.paypal.me/lovechangingtheworld">
+          <p>Donations</p>
+          <button className="btn btn-primary" target="_blank" href="https://www.paypal.me/lovechangingtheworld">
             PayPal
           </button>
           <p>
@@ -57,42 +57,6 @@ class Landing extends Component {
             donations, sponsorship, & support.... We LOVE you all.
           </p>
         </div>
-  </div>
-  )
-=======
-        <h2>Love One Another</h2>
-        <img className="logo" src={logo} height="100vh" alt="logo" />
-        <h2>Follow our Movement</h2>
-        <div className="row">
-          <div className='col-lg-3 col-md-3 col-sm-12'>
-            <a href="https://www.facebook.com/lovechangingtheworld" target="_blank">
-              <i className="fab fa-facebook-square socialmedialink"></i>
-            </a>
-            <p>Facebook</p>
-          </div>
-          <div className='col-lg-3 col-md-3 col-sm-12'>
-            <a href="https://twitter.com/LCTW_" target="_blank">
-              <i className="fab fa-twitter-square socialmedialink"></i>
-            </a>
-            <p>Twitter</p>
-          </div>
-          <div className='col-lg-3 col-md-3 col-sm-12'>
-            <a href="http://www.instagram.com/_lovechangingtheworld_" target="_blank">
-              <i className="fab fa-instagram socialmedialink"></i>
-            </a>
-            <p>Instagram</p>
-          </div>
-          <div className='col-lg-3 col-md-3 col-sm-12'>
-            <a href="https://www.snapchat.com/add/lctw1" target="_blank">
-              <i class="fab fa-snapchat-square socialmedialink"></i>
-            </a>
-            <p>Snapchat</p>
-          </div>
-        </div>
-          <h2>Donations</h2>
-        <button target="_blank" href="https://www.paypal.me/lovechangingtheworld">PayPal</button>
-          <p>Sponsors: Santa Clara County Department of Health Services, New Mission Community Church, Safeway, & Peter's Bakery.</p>
-          <p>Our #LCTW Family would like to THANK YOU for your help, donations, sponsorship, & support.... We LOVE you all.</p>
       </div>
     );
   }
--- client/src/components/pages/Donations.js
+++ client/src/components/pages/Donations.js
@@ -8,8 +8,8 @@ class Donations extends Component {
         <h1>Donations</h1>
         <p>Hello all, The Love Changing The World Family is seeking donations for the homeless and less fortunate. We are looking to make a difference in local communities and even expand our loving gestures beyond. Recently, there has been a tragic storm in Houston, Texas, and we are looking to be as helpful as possible to help the families that has suffered from loss of personal items and even homes. Remember there is no donation too small, spread the word and LOVE. There will be links to send funds to. Thank you for your generous HEARTS! We LOVE you ALL!!!</p>
         <div className="donatebuttons">
-          <button type="button" class="btn btn-primary buttonspace">PayPal</button>
-          <button type="button" class="btn btn-success buttonspace">GoFundMe</button>
+          <button type="button" className="btn btn-primary buttonspace">PayPal</button>
+          <button type="button" className="btn btn-success buttonspace">GoFundMe</button>
         </div>
       </div>
     )
