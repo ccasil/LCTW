8027e67340d7d83b86b81ecbddc7833fbc21a76f
Peter Lin <peterlin@Peters-MacBook-Air-2.local>
2018-07-25 17:21:11 -0700

    ignore readme for now, testing store functionality

--- client/src/components/pages/Donations.js
+++ client/src/components/pages/Donations.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react'
 import "../css/Donations.css";
 
+
+import Shirt from '../../images/Shop/IMG_0023.JPG';
+import Apron from '../../images/Shop/IMG_4275.JPG';
+
+
 class Donations extends Component {
   render() {
     return (
@@ -10,6 +15,86 @@ class Donations extends Component {
         <div className="donatebuttons">
           <button type="button" class="btn btn-primary buttonspace">PayPal</button>
           <button type="button" class="btn btn-success buttonspace">GoFundMe</button>
+        </div>
+ 
+
+        <div className="cartItem">
+          <h4> Shirts! </h4>
+          <img className="shoppingImg" src={Shirt}></img>
+          <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" className="cartForm">
+          <input type="hidden" name="cmd" value="_cart"></input>
+          <input type="hidden" name="business" value="H4y4shi@gmail.com"></input>
+          <input type="hidden" name="lc" value="US"></input>
+          <input type="hidden" name="item_name" value="Love Changing the World - Shirt"></input>
+          <input type="hidden" name="button_subtype" value="products"></input>
+          <input type="hidden" name="no_note" value="0"></input>
+          <input type="hidden" name="shipping" value="5.00"></input>
+          <input type="hidden" name="add" value="1"></input>
+          <input type="hidden" name="bn" value="PP-ShopCartBF:btn_cart_LG.gif:NonHostedGuest"></input>
+           
+          <div className="ShoppingOptions">
+          <table>
+            <tr><td><input type="hidden" name="on0" value="Size"></input>Size</td></tr><tr><td><select name="os0">
+              <option value="Small">Small $20.00 USD</option>
+              <option value="Medium">Medium $20.00 USD</option>
+              <option value="Large">Large $20.00 USD</option>
+              <option value="Extra-Large">Extra-Large $20.00 USD</option>
+            </select> </td></tr>
+            <tr><td><input type="hidden" name="on1" value="Color"></input>Color</td></tr><tr><td><select name="os1">
+              <option value="Red - White Text">Red - White Text </option>
+              <option value="Red - Black Text">Red - Black Text </option>
+              <option value="Black - Red Text">Black - Red Text </option>
+              <option value="Black - White Text">Black - White Text </option>
+            </select> </td></tr>
+            </table>
+            <input type="hidden" name="currency_code" value="USD"></input>
+            <input type="hidden" name="option_select0" value="Small"></input>
+            <input type="hidden" name="option_amount0" value="20.00"></input>
+            <input type="hidden" name="option_select1" value="Medium"></input>
+            <input type="hidden" name="option_amount1" value="20.00"></input>
+            <input type="hidden" name="option_select2" value="Large"></input>
+            <input type="hidden" name="option_amount2" value="20.00"></input>
+            <input type="hidden" name="option_select3" value="Extra-Large"></input>
+            <input type="hidden" name="option_amount3" value="20.00"></input>
+            <input type="hidden" name="option_index" value="0"></input>
+          </div>
+          <input type="image" className="cartButton" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+          </input><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+          </img></form>
+
+        </div>
+
+        <div className="cartItem">
+          <h4> Aprons! </h4>
+          <img className="shoppingImg" src={Apron}></img>
+          <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post" className="cartForm">
+          <div className="ShoppingOptions">
+          <input type="hidden" name="cmd" value="_cart"></input>
+          <input type="hidden" name="business" value="H4y4shi@gmail.com"></input>
+          <input type="hidden" name="lc" value="US"></input>
+          <input type="hidden" name="item_name" value="Apron"></input>
+          <input type="hidden" name="button_subtype" value="products"></input>
+          <input type="hidden" name="no_note" value="0"></input>
+          <input type="hidden" name="shipping" value="5.00"></input>
+          <input type="hidden" name="add" value="1"></input>
+          <input type="hidden" name="bn" value="PP-ShopCartBF:btn_cart_LG.gif:NonHostedGuest"></input>
+          <table>
+          <tr><td><input type="hidden" name="on0" value="Color"></input>Color</td></tr><tr><td><select name="os0">
+            <option value="White">White $25.00 USD</option>
+            <option value="Black">Black $25.00 USD</option>
+          </select> </td></tr>
+          </table>
+          </div>
+          <input type="hidden" name="currency_code" value="USD"></input>
+          <input type="hidden" name="option_select0" value="White"></input>
+          <input type="hidden" name="option_amount0" value="25.00"></input>
+          <input type="hidden" name="option_select1" value="Black"></input>
+          <input type="hidden" name="option_amount1" value="25.00"></input>
+          <input type="hidden" name="option_index" value="0"></input>
+          <input type="image" className="cartButton" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+          </input><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></img>
+          </form>
+
         </div>
       </div>
     )
---
+++ readme.txt
@@ -0,0 +1,19 @@
+//README
+
+How to Setup API.
+
+Paypal Developer Dashboard -> MY Apps & Credentials
+Get your credentials. Use postman or curl 
+
+//https://api.sandbox.paypal.com/v1/oauth2/token 
+//body x-www-urlencoded KEY: grant_Type VALUE: client_credentials
+
+
+{
+    "scope": "https://api.paypal.com/v1/payments/.* https://uri.paypal.com/services/payments/refund https://uri.paypal.com/services/applications/webhooks https://uri.paypal.com/services/payments/payment/authcapture https://uri.paypal.com/payments/payouts https://api.paypal.com/v1/vault/credit-card/.* https://uri.paypal.com/services/disputes/read-seller https://uri.paypal.com/services/subscriptions https://uri.paypal.com/services/disputes/read-buyer https://api.paypal.com/v1/vault/credit-card openid https://uri.paypal.com/services/disputes/update-seller https://uri.paypal.com/services/payments/realtimepayment",
+    "nonce": "2018-07-25T22:47:59ZBrPqB6S5iBEGrNF-tJnO79yDNGSr-jWlbJ537-LNd6Y",
+    "access_token": "A21AAGh6KsVp1OOa8hEiy0NT44s_UGS3AZNzvcWX4HWYHgqOkbVplI6zEpLMPrRCAzXXmQ2gb2qWQz9QOlum58q2ZbV1BjgcA",
+    "token_type": "Bearer",
+    "app_id": "APP-80W284485P519543T",
+    "expires_in": 32399
+}
